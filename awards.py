15b7140e8e52853539514eeefc06e30bc9d68db3
Danny Oh <dannyoh0316@gmail.com>
2021-10-26 21:30:35 -0500

    remove unused import

--- awards.py
+++ awards.py
@@ -1,4 +1,3 @@
-import json
 from queue import PriorityQueue
 from utils import normalize_text
 
--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from awards import get_awards
 def main():
     file = open('data/gg2013.json')
     data = json.load(file)
-
     awards = get_awards(data=data)
-    print(awards)
+    print(f'2013 Awards: {awards}\n')
+
+    file = open('data/gg2015.json')
+    data = json.load(file)
+    awards = get_awards(data=data)
+    print(f'2015 Awards: {awards}\n')
 
 if __name__ == '__main__':
     main()
